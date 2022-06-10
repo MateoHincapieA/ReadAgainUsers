b38a7fcf99f059463260a0d5f3379cb2dce88949
spacerose36 <mariapaulinavilla11a@gmail.com>
2022-06-09 22:04:50 -0500

    firstCommitChat

--- src/index.js
+++ src/index.js
@@ -5,3 +5,23 @@ const port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log('Server runing on port ', port);
+
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', `${socket.id.substr(0, 2)} said: ${message}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected!');
+  });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
