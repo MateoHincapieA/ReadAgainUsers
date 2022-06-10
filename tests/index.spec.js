dcfebcbad26a1c7ab1352ccfc4febccf29f9d922
MateoHincapieA <mateohincapiearias@gmail.com>
2022-06-09 21:51:19 -0500

    Integración de las listas

---
+++ tests/index.spec.js
@@ -0,0 +1,10 @@
+const app = require('../src/index.js');
+const request = require('supertest');
+
+describe('GET /user', () => {
+
+    test('should respond with a 200 status code', async () => {
+        const response = await request(app).get('/signin').send();
+        expect(response.statusCode).toBe(200);
+    });
+});
